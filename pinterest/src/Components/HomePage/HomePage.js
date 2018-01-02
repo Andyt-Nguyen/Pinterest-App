21f6d06df941e962f4cf012e1b8917fd65eb6a43
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2018-01-02 15:24:04 -0700

    Clean up for master

--- pinterest/src/Components/HomePage/HomePage.js
+++ pinterest/src/Components/HomePage/HomePage.js
@@ -7,194 +7,12 @@ import Image from './Styles/Image';
 
 class HomePage extends Component {
 
-	userSignOut() {
-		this.props.userSignOut();
-	}
-
-
 	render() {
 		return (
 			<div style={{marginTop:'20px' }}>
-			<button onClick={this.userSignOut.bind(this)}>Sign out</button>
-
 				<Masonry
 					className="my-gallery-class">
-						<ImageWrapper>
-							<Image alt="yoyyoyy" src="https://img.wennermedia.com/featured-promo-724/lacey-baker-profile-rolling-stone-2017-ec439dfb-a2ff-49e2-b85b-54750fb93b8c.jpg" />
-							<p>This is skateboarding</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
 
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://i.pinimg.com/736x/6d/08/26/6d08268c35f5f71b3e710cee25e58772--celebrity-photography-emma-watson-hair-color.jpg" />
-							<p>Emma Watson is my dream girl</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://angelsblogdotorg.files.wordpress.com/2016/03/skateboard-pic.jpg" />
-							<p>Skate Flip Dawg</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="http://www.trbimg.com/img-573233c8/turbine/la-me-freeway-skateboarding-20160510-snap" />
-							<p>More Skateboarding</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://media1.popsugar-assets.com/files/thumbor/SN1fQ4qPbYH4ApttcRzffMdVkGI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/10/25/697/n/1922564/1725c44359f0b13323c094.34428259_edit_img_image_44181220_1508940982/i/Girl-Who-Dresses-Like-Taylor-Swift.jpg" />
-							<p>Taylor Swift</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy" src="https://img.wennermedia.com/featured-promo-724/lacey-baker-profile-rolling-stone-2017-ec439dfb-a2ff-49e2-b85b-54750fb93b8c.jpg" />
-							<p>This is skateboarding</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://i.pinimg.com/736x/6d/08/26/6d08268c35f5f71b3e710cee25e58772--celebrity-photography-emma-watson-hair-color.jpg" />
-							<p>Emma Watson is my dream girl</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://angelsblogdotorg.files.wordpress.com/2016/03/skateboard-pic.jpg" />
-							<p>Skate Flip Dawg</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="http://www.trbimg.com/img-573233c8/turbine/la-me-freeway-skateboarding-20160510-snap" />
-							<p>More Skateboarding</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://media1.popsugar-assets.com/files/thumbor/SN1fQ4qPbYH4ApttcRzffMdVkGI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/10/25/697/n/1922564/1725c44359f0b13323c094.34428259_edit_img_image_44181220_1508940982/i/Girl-Who-Dresses-Like-Taylor-Swift.jpg" />
-							<p>Taylor Swift</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-						<ImageWrapper>
-							<Image alt="yoyyoyy" src="https://img.wennermedia.com/featured-promo-724/lacey-baker-profile-rolling-stone-2017-ec439dfb-a2ff-49e2-b85b-54750fb93b8c.jpg" />
-							<p>This is skateboarding</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://i.pinimg.com/736x/6d/08/26/6d08268c35f5f71b3e710cee25e58772--celebrity-photography-emma-watson-hair-color.jpg" />
-							<p>Emma Watson is my dream girl</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://angelsblogdotorg.files.wordpress.com/2016/03/skateboard-pic.jpg" />
-							<p>Skate Flip Dawg</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="http://www.trbimg.com/img-573233c8/turbine/la-me-freeway-skateboarding-20160510-snap" />
-							<p>More Skateboarding</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://media1.popsugar-assets.com/files/thumbor/SN1fQ4qPbYH4ApttcRzffMdVkGI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/10/25/697/n/1922564/1725c44359f0b13323c094.34428259_edit_img_image_44181220_1508940982/i/Girl-Who-Dresses-Like-Taylor-Swift.jpg" />
-							<p>Taylor Swift</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper><ImageWrapper>
-							<Image alt="yoyyoyy" src="https://img.wennermedia.com/featured-promo-724/lacey-baker-profile-rolling-stone-2017-ec439dfb-a2ff-49e2-b85b-54750fb93b8c.jpg" />
-							<p>This is skateboarding</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://i.pinimg.com/736x/6d/08/26/6d08268c35f5f71b3e710cee25e58772--celebrity-photography-emma-watson-hair-color.jpg" />
-							<p>Emma Watson is my dream girl</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://angelsblogdotorg.files.wordpress.com/2016/03/skateboard-pic.jpg" />
-							<p>Skate Flip Dawg</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="http://www.trbimg.com/img-573233c8/turbine/la-me-freeway-skateboarding-20160510-snap" />
-							<p>More Skateboarding</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
-
-						<ImageWrapper>
-							<Image alt="yoyyoyy"  src="https://media1.popsugar-assets.com/files/thumbor/SN1fQ4qPbYH4ApttcRzffMdVkGI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/10/25/697/n/1922564/1725c44359f0b13323c094.34428259_edit_img_image_44181220_1508940982/i/Girl-Who-Dresses-Like-Taylor-Swift.jpg" />
-							<p>Taylor Swift</p>
-							<div style={styles.saveContainer}>
-								<span className="fa fa-thumb-tack" style={styles.text}></span>
-								<span style={{color:'#fff'}}>Save</span>
-							</div>
-						</ImageWrapper>
 				</Masonry>
 			</div>
 		);
@@ -220,3 +38,181 @@ const styles = {
 };
 
 export default connect(null, { userSignOut })(HomePage);
+
+// Dummy Data
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy" src="https://img.wennermedia.com/featured-promo-724/lacey-baker-profile-rolling-stone-2017-ec439dfb-a2ff-49e2-b85b-54750fb93b8c.jpg" />
+// 	<p>This is skateboarding</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://i.pinimg.com/736x/6d/08/26/6d08268c35f5f71b3e710cee25e58772--celebrity-photography-emma-watson-hair-color.jpg" />
+// 	<p>Emma Watson is my dream girl</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://angelsblogdotorg.files.wordpress.com/2016/03/skateboard-pic.jpg" />
+// 	<p>Skate Flip Dawg</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="http://www.trbimg.com/img-573233c8/turbine/la-me-freeway-skateboarding-20160510-snap" />
+// 	<p>More Skateboarding</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://media1.popsugar-assets.com/files/thumbor/SN1fQ4qPbYH4ApttcRzffMdVkGI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/10/25/697/n/1922564/1725c44359f0b13323c094.34428259_edit_img_image_44181220_1508940982/i/Girl-Who-Dresses-Like-Taylor-Swift.jpg" />
+// 	<p>Taylor Swift</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy" src="https://img.wennermedia.com/featured-promo-724/lacey-baker-profile-rolling-stone-2017-ec439dfb-a2ff-49e2-b85b-54750fb93b8c.jpg" />
+// 	<p>This is skateboarding</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://i.pinimg.com/736x/6d/08/26/6d08268c35f5f71b3e710cee25e58772--celebrity-photography-emma-watson-hair-color.jpg" />
+// 	<p>Emma Watson is my dream girl</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://angelsblogdotorg.files.wordpress.com/2016/03/skateboard-pic.jpg" />
+// 	<p>Skate Flip Dawg</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="http://www.trbimg.com/img-573233c8/turbine/la-me-freeway-skateboarding-20160510-snap" />
+// 	<p>More Skateboarding</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://media1.popsugar-assets.com/files/thumbor/SN1fQ4qPbYH4ApttcRzffMdVkGI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/10/25/697/n/1922564/1725c44359f0b13323c094.34428259_edit_img_image_44181220_1508940982/i/Girl-Who-Dresses-Like-Taylor-Swift.jpg" />
+// 	<p>Taylor Swift</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy" src="https://img.wennermedia.com/featured-promo-724/lacey-baker-profile-rolling-stone-2017-ec439dfb-a2ff-49e2-b85b-54750fb93b8c.jpg" />
+// 	<p>This is skateboarding</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://i.pinimg.com/736x/6d/08/26/6d08268c35f5f71b3e710cee25e58772--celebrity-photography-emma-watson-hair-color.jpg" />
+// 	<p>Emma Watson is my dream girl</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://angelsblogdotorg.files.wordpress.com/2016/03/skateboard-pic.jpg" />
+// 	<p>Skate Flip Dawg</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="http://www.trbimg.com/img-573233c8/turbine/la-me-freeway-skateboarding-20160510-snap" />
+// 	<p>More Skateboarding</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://media1.popsugar-assets.com/files/thumbor/SN1fQ4qPbYH4ApttcRzffMdVkGI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/10/25/697/n/1922564/1725c44359f0b13323c094.34428259_edit_img_image_44181220_1508940982/i/Girl-Who-Dresses-Like-Taylor-Swift.jpg" />
+// 	<p>Taylor Swift</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper><ImageWrapper>
+// 	<Image alt="yoyyoyy" src="https://img.wennermedia.com/featured-promo-724/lacey-baker-profile-rolling-stone-2017-ec439dfb-a2ff-49e2-b85b-54750fb93b8c.jpg" />
+// 	<p>This is skateboarding</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://i.pinimg.com/736x/6d/08/26/6d08268c35f5f71b3e710cee25e58772--celebrity-photography-emma-watson-hair-color.jpg" />
+// 	<p>Emma Watson is my dream girl</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://angelsblogdotorg.files.wordpress.com/2016/03/skateboard-pic.jpg" />
+// 	<p>Skate Flip Dawg</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="http://www.trbimg.com/img-573233c8/turbine/la-me-freeway-skateboarding-20160510-snap" />
+// 	<p>More Skateboarding</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
+//
+// <ImageWrapper>
+// 	<Image alt="yoyyoyy"  src="https://media1.popsugar-assets.com/files/thumbor/SN1fQ4qPbYH4ApttcRzffMdVkGI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/10/25/697/n/1922564/1725c44359f0b13323c094.34428259_edit_img_image_44181220_1508940982/i/Girl-Who-Dresses-Like-Taylor-Swift.jpg" />
+// 	<p>Taylor Swift</p>
+// 	<div style={styles.saveContainer}>
+// 		<span className="fa fa-thumb-tack" style={styles.text}></span>
+// 		<span style={{color:'#fff'}}>Save</span>
+// 	</div>
+// </ImageWrapper>
