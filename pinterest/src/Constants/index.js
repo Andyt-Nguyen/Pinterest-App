542488abb122cab13f955786adacdd8dd8fe0b68
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2018-01-04 10:27:38 -0700

    Hide module when user already created there basic profile

--- pinterest/src/Constants/index.js
+++ pinterest/src/Constants/index.js
@@ -4,3 +4,4 @@ export const LOGGED_OUT = "LOGGED_OUT";
 export const SHOW_ERROR_SIGN_IN = "SHOW_ERROR_SIGN_IN";
 export const SHOW_ERROR_SIGN_UP = "SHOW_ERROR_SIGN_UP";
 export const GET_AUTH_INFO = "GET_AUTH_INFO";
+export const GET_USER_PROFILE = "GET_USER_PROFILE";
