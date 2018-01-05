c6aad562d073c5a8dcaa6b9dd8ac5f63e8861a28
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2018-01-05 01:54:27 -0700

    Module Handle Errors

--- pinterest/src/Components/Module/ImageUpload.js
+++ pinterest/src/Components/Module/ImageUpload.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import ArrowBox from './Style/ArrowBox';
 
 const ImageUpload = ({src,onChange,picText,children}) => (
-	<div style={{position:'relative'}}>
-		<ArrowBox>
-			<p>{picText}</p>
-		</ArrowBox>
+	<div>
+			<h3 style={{color:'#626065'}}>{picText}</h3>
 		<label style={styles.labelContainer}>
 			{children}
 			<div style={styles.hideInput}>
@@ -20,7 +17,8 @@ const styles = {
 	labelContainer: {
 		padding:'10px',
 		display:'flex',
-		justifyContent:'center'
+		justifyContent:'center',
+		cursor:'pointer'
 	},
 
 	hideInput: {
---
+++ pinterest/src/Components/Module/Style/ErrorBox.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.div `
+	color: red;
+	font-weight:700;
+	border: 1px solid red;
+	border-left: none;
+	border-right: none;
+`;
